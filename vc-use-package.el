slotThe/vc-use-package

commit f70bd3cf7bbe19eb35a0a6e4b6ba20a2f22a3db0

--install: Check if the car of verbatim is installed
This is always assumed to be a cons-cell.

vc-use-package.el
@@ -60,7 +60,7 @@
   "Thin wrapper around `package-vc-install'.
 This exists so we can have sane keywords arguments, yet don't
 have to go overboard when normalising."
-  (unless (package-installed-p (or verbatim name))
+  (unless (package-installed-p (or (car verbatim) name))
     (if verbatim
         (package-vc-install verbatim)
       (package-vc-install (concat fetcher repo) rev backend name))))

commit fff729d52ac10c416a40cab4593a9236eb1db945

Add vc-use-package--installed?
When forwarding the arguments to package-vc-install, we need to take
care of the fact that `verbatim' (= the `package' argument of
package-vc-install) can be the name of a package, as well as a cons-cell
of the name and a spec list.

Fixes: f70bd3cf7bbe19eb35a0a6e4b6ba20a2f22a3db0

vc-use-package.el
@@ -56,11 +56,21 @@
     :sourcehut "https://git.sr.ht/~")
   "Places from where to fetch packages.")
 
+(defun vc-use-package--installed? (verbatim name)
+  "Check if the given package is installed.
+VERBATIM are possible verbatim arguments to `package-vc-install';
+this is either just the name of the package, or a list.  In the
+latter case, the car of the list is the name of the package.  If
+VERBATIM is nil, then NAME is the name of the package."
+  (package-installed-p
+   (or (if (listp verbatim) (car verbatim) verbatim)
+       name)))
+
 (cl-defun vc-use-package--install (&key verbatim fetcher repo name rev backend)
   "Thin wrapper around `package-vc-install'.
 This exists so we can have sane keywords arguments, yet don't
 have to go overboard when normalising."
-  (unless (package-installed-p (or (car verbatim) name))
+  (unless (vc-use-package--installed? verbatim name)
     (if verbatim
         (package-vc-install verbatim)
       (package-vc-install (concat fetcher repo) rev backend name))))